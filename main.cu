273273d0211f922d5c016d6197a2462ec1122d9a
TommyDatLC <datsieusaiya12@gmail.com>
2025-10-04 06:32:37 +0700

    b2 epa

--- main.cu
+++ main.cu
@@ -1,68 +1,50 @@
 #include <iostream>
 #include <cuda.h>
-#include <stdlib.h>
-#include <ctime>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
 
 using namespace std;
-__global__ void AddInts(int *a,int *b,int count) {
-    int id = blockIdx.x * blockDim.x + threadIdx.x;
-    if (id < count)
-        a[id] += b[id];
+__device__ float CalcDistance(float3 a, float3 b) {
+    return (a.x - b.x) * (a.x - b.x)   + (a.y - b.y) * (a.y - b.y)  + (a.z  - b.z) * (a.z -b.z);
+}
+__global__ void FindClosest(float3* point,int* id,int count) {
+    if (count <= 1)
+        return;
+
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= count)
+        return;
+    float3 thispoint = point[idx];
+    float smallestSoFar = 3.4e38;
+
+    for (int i = 0;i < count;i++) {
+        if (i == idx) continue;
+
+        auto dis = CalcDistance(thispoint,point[i]);
+        if (dis < smallestSoFar) {
+            smallestSoFar = dis;
+            id[idx] = i;
+        }
+    }
 }
 int main()
 {
+    float3* point;
+    int *id;
+    int count;
 
-    // tao random seed bang cach su dung timer
-    srand(time(0));
-    int count = 100;
-    int *h_a = new int[count];
-    int *h_b = new int[count];
+
+    count = 5;
+    cudaMallocManaged(&point,sizeof(float3) * count);
+    cudaMallocManaged(&id,sizeof(int) * count);
+
     for (int i = 0;i < count;i++) {
-        h_a[i] = rand() % 1000;
-        h_b[i] = rand() % 1000;
+        point[i].x = rand() % 10;
+        point[i].y = rand() % 10;
+        point[i].z = rand() % 10;
     }
-    cout << "truoc khi cong: " << endl;
-    for (int i  =0 ;i < 5;i++) {
-        cout << h_a[i] << " " << h_b[i] << endl;
-    }
-    // chuyen bo nho tu host sang device
-    int *d_a,*d_b;
-    if (cudaMalloc(&d_a,sizeof(int) * count) != cudaSuccess) {
-        cout << "That bai de cap phat bo nho";
-        // Neu cap phat that bai thi giai phong bo nho vua moi cap phat
-        cudaFree(d_a);
-        return 0;
-    }
-    if (cudaMalloc(&d_b,sizeof(int) * count) != cudaSuccess) {
-        cout << "That bai de cap phat bo nho";
-        cudaFree(&d_b);
-        return 0;
-    }
-
-    if (cudaMemcpy(d_a,h_a,sizeof(int) * count,cudaMemcpyHostToDevice) != cudaSuccess) {
-        cout << "that bai de copy";
-        cudaFree(d_a);
-        cudaFree(d_b);
-        return 0;
-    }
-    if (cudaMemcpy(d_b,h_b,sizeof(int) * count,cudaMemcpyHostToDevice) != cudaSuccess) {
-        cout << "that bai de copy";
-        cudaFree(d_a);
-        cudaFree(d_b);
-        return 0;
-    }
-    AddInts<<<count / 256 + 1,256>>>(d_a,d_b,count);
-    if (cudaMemcpy(h_a,d_a,sizeof(int) * count,cudaMemcpyDeviceToHost) != cudaSuccess) {
-        delete[] h_a;
-        delete[] h_b;
-        cudaFree(d_a);
-        cudaFree(d_b);
-        return 0;
-    }
-    for (int i = 0;i < 5;i++) {
-        //cout << "It's" << h_a[i];
-        printf("%d ",h_a[i]);
-    }
-    delete[] h_a;
-    delete[] h_b;
+    FindClosest<<<count / 255 + 1,255>>>(point,id,count);
+    cudaDeviceSynchronize();
+    for (int i =0 ;i < count;i++)
+        cout << "Point: " << point[i].x << ' ' << point[i].y << ' ' << point[i].z << " Neareast Neighbor index: " << id[i] << '\n';
 }
